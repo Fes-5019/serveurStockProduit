c00f23abd341f012ab0d34f75734de1e6d305c8f
Yassine-5019 <yassinesa5019@gmail.com>
2020-02-04 13:12:49 +0100

    role-user

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,93 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class User implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="USER_ID")
+	private Long id;
+	
+	@Column(unique=true)
+	private String userName;
+	private String password;
+	private boolean enable;
+	
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="USERS_ROLES",
+	joinColumns={@JoinColumn(name="USER_ID")},
+	inverseJoinColumns= {@JoinColumn(name="ROLE_ID")})
+	private List<Role> roles;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isEnable() {
+		return enable;
+	}
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	public User(String userName, String password, boolean enable) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.enable = enable;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
